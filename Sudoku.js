a75ec7725a6083484b4261fdefc891b6aa13299c
Evgen Prushak <ogmprushak@mail.ru>
2021-01-08 00:06:30 +0300

    feat: add fucntion error

--- Sudoku.js
+++ Sudoku.js
@@ -8,19 +8,22 @@ class Sudoku {
       .map((x) => Number(x));
 
     this.body = [];
-
     let idCounter = 1;
+
     for (let y = 0; y < 9; y++) {
       for (let x = 0; x < 9; x++) {
         this.body.push({
           id: idCounter,
           x,
           y,
+          important: false,
           number: startValues[idCounter - 1],
           selected: false,
           supported: false,
+          error: false,
+          started: startValues[idCounter - 1] === 0 ? false : true,
           ///высчитываем сегмент, где находиться число
-          s: parseInt(y / 3) * 3 + parseInt(x / 3) * 3,
+          s: parseInt(y / 3) * 3 + parseInt(x / 3),
         });
 
         idCounter++;
@@ -30,6 +33,7 @@ class Sudoku {
 
   getRow(n) {
     const row = [];
+
     for (let i = 0; i < 9; i++) {
       row.push(this.body[9 * n + i]);
     }
@@ -39,9 +43,11 @@ class Sudoku {
 
   getColumn(n) {
     const column = [];
+
     for (let i = 0; i < 9; i++) {
       column.push(this.body[i * 9 + n]);
     }
+
     return column;
   }
 
@@ -49,6 +55,7 @@ class Sudoku {
     const segment = [];
     const x = n % 3;
     const y = parseInt(n / 3);
+
     for (let dy = 0; dy < 3; dy++) {
       for (let dx = 0; dx < 3; dx++) {
         segment.push(this.body[y * 27 + dy * 9 + x * 3 + dx]);
@@ -59,57 +66,125 @@ class Sudoku {
   }
 
   keydownHandler(event, cell) {
-    if ("123456789".includes(event.key)) {
-      cell.number = parseInt(event.key);
-      // отменяет стандартную обработку событий
-    } else if (["Backspace", "Delete"].includes(event.key)) {
-        cell.number = 0;
-    }
+    if (!cell.started) {
+      if ("123456789".includes(event.key)) {
+        cell.number = parseInt(event.key);
 
+        if (cell.error) {
+            for (const item of this.body) {
+                item.error = false;
+            }
+        }
+
+        for (const item of this.getRow(cell.y)) {
+          if (item === cell) {
+            continue;
+          }
+
+          if (item.number === cell.number) {
+            item.error = true;
+            cell.error = true;
+          }
+        }
+
+        for (const item of this.getColumn(cell.x)) {
+          if (item === cell) {
+            continue;
+          }
+
+          if (item.number === cell.number) {
+            item.error = true;
+            cell.error = true;
+          }
+        }
+
+        for (const item of this.getSegment(cell.s)) {
+          if (item === cell) {
+            continue;
+          }
+
+          if (item.number === cell.number) {
+            item.error = true;
+            cell.error = true;
+          }
+        }
+      } else if (["Backspace", "Delete"].includes(event.key)) {
+        cell.number = 0;
+      }
+
+      for (const item of this.body) {
+        item.important = false;
+      }
+
+      if (cell.number) {
+        for (const item of this.body) {
+          if (item.number === cell.number) {
+            item.important = true;
+          }
+        }
+      }
+    }
+    // отменяет стандартную обработку событий
     event.preventDefault();
     this.viewUpdate();
   }
 
   focusHandler(event, cell) {
     cell.selected = true;
+
     for (const item of this.getRow(cell.y)) {
       item.supported = true;
     }
+
     for (const item of this.getColumn(cell.x)) {
       item.supported = true;
     }
+
     this.viewUpdate();
   }
 
   blurHandler(event, cell) {
     cell.selected = false;
-    for (const item of this.getRow(cell.y)) {
-      item.supported = false;
+    if (cell.error) {
+      cell.number = 0;
     }
-    for (const item of this.getColumn(cell.x)) {
-      item.supported = false;
+
+    for (const cell of this.body) {
+      cell.important = false;
+      cell.supported = false;
+      cell.error = false;
     }
+
     this.viewUpdate();
   }
 
   getHTML(size) {
     // создание cell элементов
-    for (const item of this.body) {
+    for (const cell of this.body) {
       const inputElement = document.createElement("input");
+
       inputElement.classList.add("sudoku-cell");
       inputElement.setAttribute("type", "text");
+
       inputElement.addEventListener("keydown", (event) =>
-        this.keydownHandler(event, item)
-      );
-      inputElement.addEventListener("focus", (event) =>
-        this.focusHandler(event, item)
-      );
-      inputElement.addEventListener("blur", (event) =>
-        this.blurHandler(event, item)
+        this.keydownHandler(event, cell)
       );
 
-      item.element = inputElement;
+      inputElement.addEventListener("focus", (event) =>
+        this.focusHandler(event, cell)
+      );
+
+      inputElement.addEventListener("blur", (event) =>
+        this.blurHandler(event, cell)
+      );
+
+      if (cell.started) {
+        inputElement.classList.add("start-cell");
+      }
+
+      cell.element = inputElement;
     }
+
     const rootElement = document.createElement("div");
     rootElement.classList.add("sudoku-game");
     rootElement.style.width = `${size}px`;
@@ -124,9 +199,9 @@ class Sudoku {
       for (const cell of this.getSegment(s)) {
         segmentElement.append(cell.element);
       }
+
       rootElement.append(segmentElement);
     }
-
 
     this.viewUpdate();
     return rootElement;
@@ -134,14 +209,29 @@ class Sudoku {
 
   viewUpdate() {
     for (const cell of this.body) {
-      cell.element.classList.remove("supported-cell", "selected-cell");
+      cell.element.classList.remove(
+        "error",
+        "important-cell",
+        "supported-cell",
+        "selected-cell"
+      );
+
       cell.element.value = cell.number ? cell.number : "";
+
       if (cell.supported) {
         cell.element.classList.add("supported-cell");
       }
 
       if (cell.selected) {
         cell.element.classList.add("selected-cell");
+      }
+
+      if (cell.error) {
+        cell.element.classList.add("error");
+      }
+
+      if (cell.important) {
+        cell.element.classList.add("important-cell");
       }
     }
   }
